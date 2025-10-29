66920134e9828212ff7768fa42646b5c39ebfe93
sizhens <126198288+sizhens@users.noreply.github.com>
2025-10-29 19:58:30 -0400

    Make zoom buttons round and green

--- mapify_svelte/src/Map.svelte
+++ mapify_svelte/src/Map.svelte
@@ -132,4 +132,9 @@
         box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
         position: relative;
     }
+
+    .leaflet-control-zoom {
+        border-radius: 10px;
+        background-color: rgba(143, 188, 143, 0.8);
+    }
 </style>
