c9b56592ad51d36754def77596ae228f5de759c0
sizhens <126198288+sizhens@users.noreply.github.com>
2025-10-29 00:24:58 -0400

    Change height

--- mapify_svelte/src/Map.svelte
+++ mapify_svelte/src/Map.svelte
@@ -91,7 +91,7 @@
                 width:220px;
                 height:300px;
                 left:{pinFormPosition.x - 110}px;
-                top:{pinFormPosition.y - 150}px; z-index:1000;"
+                top:{pinFormPosition.y - 130}px; z-index:1000;"
             >
                 <PinForm
                     lat={newPinCoords.lat}
